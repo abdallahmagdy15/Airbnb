3f34f65f1ba35dda96c076d08ff987c921cc4bbe
hassanyahia <65390431+hassanyahia@users.noreply.github.com>
2021-04-29 02:02:35 +0200

    Property

---
+++ Models/Property/Property.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.Models.Property
+{
+    public class Property
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Range(1, 50)]
+        public int Number_Of_BedRooms { set; get; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Range(1, 50)]
+        public int Number_Of_Beds { set; get; }
+
+        [Required]
+        [MinLength(3)]
+        public string Title { set; get; }
+        [MinLength(10)]
+        public string Description { set; get; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Price { set; get; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        public int Number_Of_Days_Advanced { set; get; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Number_Of_Days_Notice { set; get; }
+
+        [Required]
+        public DateTime Start_Booking_Date { set; get; }
+
+        [Required]
+        public DateTime End_Booking_Date { set; get; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Max_stay { set; get; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Min_stay { set; get; }
+
+        [Required]
+        public DateTime Date { set; get; }
+    }
+}
---
+++ Models/Property/Property_UnavailableDays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.Models.Property
+{
+    public class Property_UnavailableDays
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int Id { set; get; }
+        [ForeignKey("Property")]
+        [Key]
+        [Column(Order = 2)]
+        public int Property_Id { set; get; }
+
+        [Required]
+        public DateTime Unavailable_Days { set; get; }
+
+        public Property Property { get; set; }
+    }
+}
