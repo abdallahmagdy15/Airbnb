58e6ca71c17ff6d9ea8f47a5c3637b9b0e487fb5
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-05-02 09:58:43 +0200

    add Adminstration controller to create new roles

---
+++ ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public String RoleName { get; set; }
+    }
+}
