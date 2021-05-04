41b7a060a98badfac530d14d6d9caaf131d61671
NotAhmedNasr <ahmed0alinasr@gmail.com>
2021-05-04 23:16:41 +0200

    prop details view

---
+++ Controllers/PropertyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.Controllers
+{
+    public class PropertyController : Controller
+    {
+        public IActionResult PropertyDetails()
+        {
+            return View("/views/Guest/PropertyDetails.cshtml");
+        }
+    }
+}
