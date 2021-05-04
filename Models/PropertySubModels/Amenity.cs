f435b45286f455ade8b581d1883fd1d881f63c91
NotAhmedNasr <ahmed0alinasr@gmail.com>
2021-05-04 22:04:13 +0200

    icons to amenity

--- Models/PropertySubModels/Amenity.cs
+++ Models/PropertySubModels/Amenity.cs
@@ -11,6 +11,9 @@ namespace Airbnb.Models.PropertySubModels
         [StringLength(200, MinimumLength = 2)]
         public string Name { get; set; }
 
+        [StringLength(5000)]
+        public string Icon { get; set; }
+
         public virtual List<PropertyAmenity> Properties { get; set; }
     }
 }
