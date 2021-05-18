1606a7ff647f002fa72cac3652cb25ff13758b2f
hassanyahia <65390431+hassanyahia@users.noreply.github.com>
2021-05-19 01:09:51 +0200

    first screen

---
+++ Controllers/Listing/ListingController.cs
@@ -0,0 +1,32 @@
+﻿using Airbnb.Data;
+using Airbnb.Models.PropertySubModels;
+using Airbnb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.Controllers.Listing
+{
+    public class ListingController : Controller
+    {
+       public IActionResult mona()
+        {
+            return View();
+        }
+        public IActionResult KindOfPlace()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult KindOfPlace(KindOfPlace kindOfPlace)
+        {
+           
+            
+
+            return RedirectToAction("mona");
+        }
+    }
+}
---
+++ ViewModels/KindOfPlace.cs
@@ -0,0 +1,14 @@
+﻿using Airbnb.Models.PropertySubModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.ViewModels
+{
+    public class KindOfPlace
+    {
+        public string Categoryname { get; set; }
+        public string GuestPlaceTypeName { get; set; }
+    }
+}
