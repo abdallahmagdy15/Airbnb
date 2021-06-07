1624d051578dd38513483b2af5e97757b4f7cab2
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-06-07 16:39:44 +0200

    Adding messaging models

---
+++ Models/Messaging/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.Models.Messaging
+
+{
+    public class Chat
+    {
+        public int ChatId { get; set; }
+        public ICollection<Message> Messages{ get; set; }
+        public ICollection<AppUser> Users { get; set; }
+
+    }
+}
---
+++ Models/Messaging/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.Models.Messaging
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Text { get; set; }
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+        public int ChatId { get; set; }
+        public Chat Chat { get; set; }
+    }
+}
