a2aa18aeec80374dc32e96c245b3c2a7656a6416
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-06-08 21:32:53 +0200

    adding MessagingController with action methods

---
+++ Controllers/MessagingController.cs
@@ -0,0 +1,40 @@
+﻿using Airbnb.Models.Messaging;
+using Airbnb.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.Controllers
+{
+    public class MessagingController : Controller
+    {
+        private readonly IMessagingService messagingService;
+
+        public MessagingController(IMessagingService messagingService)
+        {
+            this.messagingService = messagingService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var chats = await messagingService.GetMyChats();
+            return View(chats);
+        }
+        public async Task<IActionResult> GetChatById(int chatid)
+        {
+            await messagingService.RemoveChat(chatid);
+            return Ok();
+        }
+        public async Task<ActionResult<Chat>> GetChatWith(string recieverid)
+        {
+            return Ok(await messagingService.GetChatWith(recieverid));
+        }
+
+        public async Task<IActionResult> RemoveChat(int chatId)
+        {
+            await messagingService.RemoveChat(chatId);
+            return Ok();
+        }
+    }
+}
