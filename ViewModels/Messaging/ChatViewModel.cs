199dc6dae5095042d67c77a4cf4177a279f1a2ac
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-06-12 17:44:10 +0200

    Messaging View UI changes...

---
+++ ViewModels/Messaging/ChatViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Airbnb.Models;
+using Airbnb.Models.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airbnb.ViewModels.Messaging
+{
+    public class ChatViewModel
+    {
+        public Chat Chat { get; set; }
+        public AppUser CurrentUser { get; set; }
+    }
+}
