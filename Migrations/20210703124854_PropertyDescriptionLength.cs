9349f7888a390cfdcd4ce68d092039524b9d43aa
Soliman Zaid <77305491+SolimanZaid10@users.noreply.github.com>
2021-07-03 14:50:28 +0200

    PropertyDescriptionLength

---
+++ Migrations/20210703124854_PropertyDescriptionLength.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Airbnb.Migrations
+{
+    public partial class PropertyDescriptionLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Properties",
+                type: "nvarchar(max)",
+                maxLength: 10000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Properties",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 10000,
+                oldNullable: true);
+        }
+    }
+}
